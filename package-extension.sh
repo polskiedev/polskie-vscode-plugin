a07c3cb6b1849852a5adb44e41c47c8c596cf9f2
John Paul Fernandez <johnpaul.fernandez@newbook.cloud>
2024-08-07 14:27:34 +0800

    DEV-000001: Clean up

--- package-extension.sh
+++ package-extension.sh
@@ -4,15 +4,11 @@
 # Define the version increment type (e.g., patch, minor, major)
 VERSION_TYPE=${1:-patch}
 
-# Increment version in package.json
-npm version $VERSION_TYPE -m "Bump version to %s"
-
-npm run compile
-
 OUTPUT_DIR="compiled/"
 if [ ! -d "$OUTPUT_DIR" ]; then
 	mkdir "$OUTPUT_DIR"
 fi
 
 # Move the .vsix file to the specified folder
-vsce package && LATEST_VSIX=$(ls *.vsix | sort -V | tail -n 1) && mv "$LATEST_VSIX" "$OUTPUT_DIR/" && echo "Compiled" 
+# Increment version in package.json
+npm version $VERSION_TYPE -m "Bump version to %s" && npm run compile && vsce package && LATEST_VSIX=$(ls *.vsix | sort -V | tail -n 1) && mv "$LATEST_VSIX" "$OUTPUT_DIR/" && echo "Compiled" 
